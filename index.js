1ea3a7bf889b4797d737360bb673372e9ab41fa9
Victor Orlunda <vicorlunda@gmail.com>
2022-02-19 04:53:44 +0100

    implement bot functionality

---
+++ index.js
@@ -0,0 +1,112 @@
+/**
+ *
+ * Title: Hello Telegram Bot
+ * Author: Victor Orlunda
+ * Date: 19 Feb, 2022
+ *
+ */
+
+/**
+ *
+ * Required External Modules
+ *
+ */
+
+import * as dotenv from 'dotenv';
+import { Telegraf } from 'telegraf';
+
+/**
+ *
+ * Initialize App Variables
+ *
+ */
+
+dotenv.config();
+
+/**
+ *
+ * App Main Functiion
+ *
+ */
+
+function main() {
+  console.log('started!');
+
+  const bot = new Telegraf(process.env.BOT_TOKEN);
+
+  bot.start(ctx => {
+    ctx.reply(
+      `${randomReply()} Welcome! Type "hello" or send a sticker to get a response!`
+    );
+  });
+
+  bot.help(ctx => {
+    ctx.reply(
+      `${randomReply()} This bot is a simple conversational bot with limited capabilities.`,
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: 'contact me',
+                callback_data: 'contact-me',
+              },
+              {
+                text: 'hello',
+                callback_data: 'hello',
+              },
+            ],
+          ],
+        },
+      }
+    );
+  });
+
+  bot.action('contact-me', ctx => {
+    ctx.reply(
+      `${randomReply()} Nice getting intouch with you, my username is @@hiatus_0`
+    );
+  });
+
+  bot.action('hello', ctx => {
+    ctx.reply(`${randomReply()} I think you're awesome! 😇`);
+  });
+
+  bot.on('sticker', ctx => {
+    ctx.reply(`${randomReply()} Nice sticker! ✨`);
+  });
+
+  bot.hears('hello', ctx => {
+    ctx.reply(`${randomReply()} I think you're awesome! 😇`);
+  });
+
+  bot.launch();
+
+  process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+  process.once('SIGINT', () => bot.stop('SIGINT'));
+}
+
+/**
+ *
+ * App Activation
+ *
+ */
+
+main();
+
+/**
+ *
+ * Helper Functions
+ *
+ */
+
+function randomReply() {
+  const index = Math.floor(Math.random() * 3);
+
+  const replies = ['Hi 👋🏼', 'Hey 👋🏼', 'Hey ya 👋🏼'];
+
+  const randomReply = replies[index];
+
+  return randomReply;
+}
